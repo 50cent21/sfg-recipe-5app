2f063cf3914b47f0d5cfe92a089daf83933e5a9c
Tomislav Funtek <tomislav.funtek007@gmail.com>
2022-01-23 13:55:28 +0100

    Added OneToOne unidirectional relationship between Ingredient and UOM

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	
+	private BigDecimal amount;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public UnitOfMeasure getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+
+	public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+		this.unitOfMeasure = unitOfMeasure;
+	}
+	
+	
+}
